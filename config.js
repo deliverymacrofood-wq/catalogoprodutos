b9d3797c519405f460bdf1044d75fe6cca1a08be
deliverymacrofood-wq <deliverymacrofood@gmail.com>
2026-08-25 10:31:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Cole aqui os dados do seu projeto Supabase.
 // Supabase > Project Settings > API
-window.SUPABASE_URL = "https://pyfokwuyzdbeiyzcmxhk.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://pyfokwuyzdbeiyzcmxhk.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Zm9rd3V5emRiZWl5emNteGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MzY2MzcsImV4cCI6MjEwMzIxMjYzN30.8yi7TNNK-fVSBqkpVwNgRGn8asKwFL_7JXB0A5s62UE";
